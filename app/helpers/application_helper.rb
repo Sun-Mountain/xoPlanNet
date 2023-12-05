685133dead0a4fc00d14d41e2c29649986a8472b
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-12-05 15:20:52 -0500

    reset

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def current_page(path, link_class) # rubocop:disable Metrics/AbcSize
-    if request.path == path
-      link_class + ' active'
-    else
-      link_class
-    end
-  end
-end
