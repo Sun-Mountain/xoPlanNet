40b19ce0b6f8da1055cd6a36eba913d992ec4ddd
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-12-05 14:59:30 -0500

    add rspec

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  EMAIL_REGEXP = /\A[^@\s]+@[^@\s]+\z/
+
+  has_secure_password
+
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true, on: :update
+  validates :password, presence: {on: create}, length: {minimum: 8, maximum: 128}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, format: EMAIL_REGEXP
+
+  normalizes :email, with: -> { _1.strip.downcase }
+
+  ### Name
+  def name=(full_name)
+    self.first_name, self.last_name = full_name.to_s.squish.split(/\s/, 2)
+  end
+
+  def name
+    [first_name, last_name].join(" ")
+  end
+
+  def to_s
+    name
+  end
+end
