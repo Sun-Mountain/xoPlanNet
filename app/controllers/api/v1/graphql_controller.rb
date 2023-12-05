1f82739a5e18be24ee1f51a3ed3e7714366a169d
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-12-05 14:23:12 -0500

    set routes

---
+++ app/controllers/api/v1/graphql_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GraphqlController < ApplicationController
+      include GraphqlDevise::SetUserByToken
+      include ActionController::Cookies
+
+      def graphql
+        result = DummySchema.execute(params[:query], **execute_params(params))
+
+        render json: result unless performed?
+      end
+
+      def interpreter
+        render json: InterpreterSchema.execute(params[:query], **execute_params(params))
+      end
+
+      private
+
+      def execute_params(item)
+        {
+          operation_name: item[:operationName],
+          variables:      ensure_hash(item[:variables]),
+          context:        gql_devise_context(SchemaUser, User)
+        }
+      end
+
+      def ensure_hash(ambiguous_param)
+        case ambiguous_param
+        when String
+          if ambiguous_param.present?
+            ensure_hash(JSON.parse(ambiguous_param))
+          else
+            {}
+          end
+        when Hash, ActionController::Parameters
+          ambiguous_param
+        when nil
+          {}
+        else
+          raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
+        end
+      end
+
+      def verify_authenticity_token
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,26 @@ Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post "/graphql", to: "graphql#execute"
 
-  mount_graphql_devise_for 'User', at: 'auth'
+  mount_graphql_devise_for(
+    'User',
+    at: '/api/v1/graphql_auth',
+    # base_controller: CookiesController,
+    # operations: { login: Mutations::Login, register: Mutations::Register },
+    # additional_mutations: { register_confirmed_user: Mutations::RegisterConfirmedUser },
+    # additional_queries: { public_user: Resolvers::PublicUser }
+  )
+
+  mount_graphql_devise_for(
+    'Admin',
+    # authenticatable_type: Types::CustomAdminType,
+    # skip:                 [:register],
+    # operations:           {
+    #   update_password_with_token: Mutations::ResetAdminPasswordWithToken
+    # },
+    at: '/api/v1/admin/graphql_auth'
+  )
+
+  mount_graphql_devise_for 'Admin', at: 'api/auth'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
